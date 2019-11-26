175dc7014a4c6d27d3c06f2f6a4e9d41eaf13746
Bryce Champaign <bechampaign@protonmail.com>
2019-11-26 12:31:31 -0500

    Init

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
+
+module.exports = config;

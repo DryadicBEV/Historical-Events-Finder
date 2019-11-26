f97ebb3fd2d7039006735b3993889943cadf2f6e
David <david.lam296@gmail.com>
2019-11-26 16:26:50 -0500

    Create skeleton for react front-end

---
+++ src/client/components/Result.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class Result extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Result Component</h2>
+      </div>
+    );
+  }
+}

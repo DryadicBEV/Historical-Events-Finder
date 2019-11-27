a7f9a9b2bb03a12bdbc9edb64e564725eaddefd3
Bryce Champaign <bechampaign@protonmail.com>
2019-11-27 14:06:15 -0500

    Refactor and fix search functionality

--- src/client/components/Result.jsx
+++ src/client/components/Result.jsx
@@ -1,54 +1,23 @@
-import React from "react";
-import NavButton from "./NavButton.jsx";
-import Event from "./Event.jsx";
+import React from 'react';
+import NavButton from './NavButton.jsx';
+import Event from './Event.jsx';
 
-export default class Result extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      page: 1
-    };
+const Result = props => (
+  <>
+    <div id="events-container">
+      {props.results.map((result, idx) => (
+        <Event key={idx} date={result.date} description={result.description} />
+      ))}
+    </div>
+    <div className="button-container">
+      <NavButton handleClick={props.handlePageDecrement} page={props.page} />
+      <NavButton
+        handleClick={props.handlePageIncrement}
+        increment={true}
+        display={props.results.length > 0 ? true : false}
+      />
+    </div>
+  </>
+);
 
-    this.handlePageIncrement = this.handlePageIncrement.bind(this);
-    this.handlePageDecrement = this.handlePageDecrement.bind(this);
-  }
-
-  handlePageIncrement() {
-    this.setState({ page: this.state.page + 1 }, () => {
-      this.props.search(this.props.currentQuery, this.state.page);
-    });
-  }
-
-  handlePageDecrement() {
-    this.setState({ page: this.state.page - 1 }, () => {
-      this.props.search(this.props.currentQuery, this.state.page);
-    });
-  }
-
-  render() {
-    return (
-      <>
-        <div id="events-container">
-          {this.props.results.map((result, idx) => (
-            <Event
-              key={idx}
-              date={result.date}
-              description={result.description}
-            />
-          ))}
-        </div>
-        <div className="button-container">
-          <NavButton
-            handleClick={this.handlePageDecrement}
-            page={this.state.page}
-          />
-          <NavButton
-            handleClick={this.handlePageIncrement}
-            increment={true}
-            display={this.props.results.length > 0 ? true : false}
-          />
-        </div>
-      </>
-    );
-  }
-}
+export default Result;
